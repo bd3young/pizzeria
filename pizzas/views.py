016811c636b8271011fcca6318917396f12447c2
Beau DeYoung <beaudeyoung@gmail.com>
2021-03-30 23:20:11 -0400

    pizza and toppings

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Pizza, Topping 
 
-# Create your views here.
+def index(request):
+        return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.all()
+    context = {'pizzas' : pizzas}
+    return render (request, 'pizzas/pizzas.html', context)
+
+def toppings(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    toppings = pizza.topping_set.order_by()
+    context = {'pizza' : pizza, 'toppings' : toppings}
+    return render (request, 'pizzas/toppings.html', context)
